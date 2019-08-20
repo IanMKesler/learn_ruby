4143f2911a06f3e5a1892bef0d3a5f8f17132f70
Ian Kesler <ianmkesler@gmail.com>
2019-08-20 15:20:00 -0700

    solution to books

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,32 @@
 class Book
-# write your code here
+  def title
+    @title
+  end
+
+  def title=(string)
+    string = string.split(" ")
+    string = capitalize(string)
+    @title = string.join(" ")
+  end
+
+  def capitalize(string)
+    string = string.map.with_index { |word, index|
+      if index == 0 || word == "i" || check(word)
+        word = word[0].upcase + word[1..-1]
+      else
+        word = word
+      end
+    }
+    string
+  end
+
+  def check(word)
+    exceptions = { articles: ["the", "a", "an"], conjunctions: ["and"], prepositions: ["in", "of"] }
+    exceptions.each do |type, list|
+      if list.include? word
+        return false
+      end
+    end
+    return true
+  end
 end
