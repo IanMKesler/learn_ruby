0df6054f73c07fb8cc6a071c40ed9605ed04b9dd
Ian Kesler <ianmkesler@gmail.com>
2019-08-19 20:06:48 -0700

    solution to calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
 #write your code here
+def add (a,b)
+    a+b
+end
+
+def subtract (a,b)
+    a-b
+end
+
+def sum (array)
+    sum = 0
+    array.each do |num|
+        sum += num
+    end
+    sum
+end
+
+def multiply (array)
+    product = 1
+    array.each do |num|
+        product = product*num
+    end
+    product
+end
+
+def power (a,b)
+    a**b
+end
+
+def factorial (num)
+    if num == 0
+        return 1
+    else
+        return num*factorial(num-1)
+    end
+end
