dae9234190110ed18a21a762a78d551167427018
Ian Kesler <ianmkesler@gmail.com>
2019-08-20 15:48:09 -0700

    solution to timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,34 @@
 class Timer
-  #write your code here
+  def initialize
+    @seconds = 0
+    @minutes = 0
+    @hours = 0
+  end
+
+  def seconds 
+    @seconds
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def time_string
+    increment
+    "#{format(@hours)}:#{format(@minutes)}:#{format(@seconds)}"
+  end
+
+  def increment
+    @hours = @seconds/3600
+    @seconds = @seconds%3600
+
+    @minutes = @seconds/60
+    @seconds = @seconds%60
+  end
+
+  def format(number)
+    first_digit = (number / 10).to_s
+    second_digit = (number % 10).to_s
+    first_digit + second_digit
+  end
 end
